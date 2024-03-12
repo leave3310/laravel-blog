6013e9ccf221deaf740d5a1b45828f56afb091b4
leave3310 <sampisam3@gmail.com>
2024-03-12 15:46:47 +0800

    feat: 新增軟刪功能

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Article extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
 
     protected $fillable = ['title', 'user_name', 'user_email', 'content'];
 
